7afb1bfea985f36eee7aa51dfad7a481f98628ea
David8751 <davidlunapocket@gmail.com>
2026-07-16 10:22:32 -0300

    Update teste-seimples.js

--- teste-seimples.js
+++ teste-seimples.js
@@ -3,19 +3,19 @@ const assert = require('assert');
 console.log("Iniciando testes automatizados da Pokédex...");
 
 try {
-    // Teste 1: Matemática básica (simulando uma regra de negócio)
-    assert.strictEqual(1 + 1, 3);
-    console.log("✅ Teste 1 passou: A lógica matemática está correta!");
+    // Teste de  Matemática básica 
+    assert.strictEqual(1 + 1, 2);
+    console.log(" Teste 1 passou");
 
     // Teste 2: Simulação de dados
-    const pokemon = "Pikachu";
+    const pokemon = "Pikach";
     assert.strictEqual(typeof pokemon, "string");
-    console.log("✅ Teste 2 passou: O dado recebido tem o formato correto!");
+    console.log("Teste 2 passou");
 
-    console.log("🚀 Todos os testes passaram com sucesso!");
-    process.exit(0); // O código 0 avisa o GitHub que o teste passou
+    console.log("Todos os testes passaram com sucesso!");
+    process.exit(0); 
 } catch (error) {
-    console.error("❌ O teste falhou. Bug encontrado!");
+    console.error(" O teste falhou");
     console.error(error);
-    process.exit(1); // O código 1 avisa o GitHub para reprovar e ficar vermelho
+    process.exit(1); 
 }

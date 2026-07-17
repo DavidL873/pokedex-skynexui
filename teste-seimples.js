cedea609030de5e1013416d43c76d148e5db21fe
David8751 <davidlunapocket@gmail.com>
2026-07-17 08:46:25 -0300

    Update teste-seimples.js

--- teste-seimples.js
+++ teste-seimples.js
@@ -3,16 +3,16 @@ const assert = require('assert');
 console.log("Iniciando testes automatizados da Pokédex...\n");
 
 try {
-    // Teste 1: Matemática básica (Corrigido)
+    
     assert.strictEqual(1 + 1, 2, "Erro");
     console.log(" Teste 1 passou");
 
-    // Teste 2: Verificando o tamanho de uma lista (Array)
+  
     const pokemonsIniciais = ['Bulbasaur', 'Charmander', 'Squirtle'];
     assert.strictEqual(pokemonsIniciais.length, 3, "Erro");
     console.log(" Teste 2 passou:");
 
-    // Teste 3: Verificando uma propriedade de um objeto (String)
+    
     const pikachu = { nome: 'Pikachu', tipo: 'Elétrico' };
     assert.strictEqual(pikachu.tipo, 'Elétrico', "Erro");
     console.log(" Teste 3 passou");
